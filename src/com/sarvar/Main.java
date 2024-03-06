dfae757406ee6bf7a27038b7e5527570a85c054c
Sarvar55 <sarvar.musazade@gmail.com>
2024-03-07 00:55:21 +0300

    Fix: Change loop range to 12

--- src/com/sarvar/Main.java
+++ src/com/sarvar/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 12; i++) {
             System.out.println("hello world");
         }
     }
