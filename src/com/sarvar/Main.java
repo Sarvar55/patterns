2dc8fe34891ff6f1d508eb0eecaf6d1799eba540
Sarvar55 <sarvar.musazade@gmail.com>
2024-03-07 01:13:57 +0300

    fix: I guess it happened but

--- src/com/sarvar/Main.java
+++ src/com/sarvar/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         for (int i = 0; i < 21
                 ; i++) {
-            System.out.println("hello world" + i);
+            System.out.println("hello sarvar" + i);
         }
     }
 }
